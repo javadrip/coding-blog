1efaf1fcec3ad3cfbc62a2b68c5f8560973a350c
Vinleon Ang <javadripdev@gmail.com>
2023-01-20 23:53:08 +0800

    Sanity initial set up

---
+++ sanity.config.ts
@@ -0,0 +1,19 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemas'
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+
+export default defineConfig({
+  basePath: "/studio",
+  name: 'Coding_blog_content_studio',
+  title: 'Coding blog content studio',
+  projectId,
+  dataset,
+  plugins: [deskTool(), visionTool()],
+  schema: {
+    types: schemaTypes,
+  },
+})
