a6097efdef513dbe2129b225f62a8290c8399d8e
Vinleon Ang <javadripdev@gmail.com>
2023-01-22 17:24:26 +0800

    Build banner component

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,7 +1,3 @@
 export default function HomePage() {
-  return(
-    <div>
-      <h1 className="text-4xl">Decoding the coder's life</h1>
-    </div>
-  )
+  return <div></div>;
 }
---
+++ components/Banner.tsx
@@ -0,0 +1,16 @@
+function Banner() {
+  return (
+    <div className="flex flex-col lg:flex-row lg:space-x-5 justify-between font-bold px-10 py-5 mb-10">
+      <div>
+        <h1 className="text-6xl">The Code Life</h1>
+        <h2 className="text-3xl mt-5 md:mt-0">Decoding the coder's life</h2>
+      </div>
+      <p className="mt-5 md:mt-2 text-gray-400 max-w-sm">
+        New product features | The latest in technology | The weekly debugging
+        nightmares and more!
+      </p>
+    </div>
+  );
+}
+
+export default Banner;
