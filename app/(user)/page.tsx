391e90c76e0ecf8d8c03793dced482a85a4d094c
Vinleon Ang <javadripdev@gmail.com>
2023-01-25 17:11:49 +0800

    Create BlogList component

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,13 +1,37 @@
 import { previewData } from "next/headers";
+import { groq } from "next-sanity";
+import { client } from "../../lib/sanity.client";
+import PreviewSuspense from "../../components/PreviewSuspense";
+import PreviewBlogList from "../../components/PreviewBlogList";
+import BlogList from "../../components/BlogList";
 
-export default function HomePage() {
+const query = groq`
+  *[_type == "post"] {
+    ...,
+    author->,
+    categories[]->
+  } | order(_createdAt desc)
+`;
+
+export default async function HomePage() {
   if (previewData()) {
-    return <div>Preview mode</div>;
+    return (
+      <PreviewSuspense
+        fallback={
+          <div role="status">
+            <p className="text-center text-lg animate-pulse text-[#F7AB0A]">
+              Loading preview...
+            </p>
+          </div>
+        }
+      >
+        <PreviewBlogList query={query} />
+      </PreviewSuspense>
+    );
   }
 
-  return (
-    <div>
-      <h1>Not in preview mode</h1>
-    </div>
-  );
+  const posts = await client.fetch(query);
+  console.log(posts);
+
+  return <BlogList posts={posts} />;
 }
