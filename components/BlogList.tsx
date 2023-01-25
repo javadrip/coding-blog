0ba05f0eae0212abc968b34cb734573d898b70e6
Vinleon Ang <javadripdev@gmail.com>
2023-01-25 19:13:05 +0800

    Add title, description and category to BlogList

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -21,6 +21,31 @@ function BlogList({ posts }: Props) {
                 alt={post.author.name}
                 fill
               />
+              <div className="absolute bottom-0 w-full bg-opacity-20 bg-black backdrop-blur-lg rounded drop-shadow-lg text-white p-5 flex justify-between">
+                <div>
+                  <p className="font-bold">{post.title}</p>
+                  <p>
+                    {new Date(post._createdAt).toLocaleDateString("en-GB", {
+                      day: "numeric",
+                      month: "long",
+                      year: "numeric",
+                    })}
+                  </p>
+                </div>
+
+                <div className="flex flex-col md:flex-row gap-y-2 md:gap-x-2 items-center">
+                  {post.categories.map((category) => (
+                    <div className="bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold">
+                      <p>{category.title}</p>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+
+            <div className="mt-5 flex-1">
+              <p className="underline text-lg font-bold">{post.title}</p>
+              <p className="text-gray-500">{post.description}</p>
             </div>
           </div>
         ))}
