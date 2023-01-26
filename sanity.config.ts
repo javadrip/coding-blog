6949cebfaaee7f6bf1f1c646f8448233a3baa263
Vinleon Ang <javadripdev@gmail.com>
2023-01-26 11:36:09 +0800

    Install iframe pane and set up post preview

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,19 +1,23 @@
-import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemas'
+import { defineConfig } from "sanity";
+import { deskTool } from "sanity/desk";
+import { visionTool } from "@sanity/vision";
+import { schemaTypes } from "./schemas";
+import { getDefaultDocumentNode } from "./structure";
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
 
 export default defineConfig({
   basePath: "/studio",
-  name: 'Coding_blog_content_studio',
-  title: 'Coding blog content studio',
+  name: "Coding_blog_content_studio",
+  title: "Coding blog content studio",
   projectId,
   dataset,
-  plugins: [deskTool(), visionTool()],
+  plugins: [
+    deskTool({ defaultDocumentNode: getDefaultDocumentNode }),
+    visionTool(),
+  ],
   schema: {
     types: schemaTypes,
   },
-})
+});
